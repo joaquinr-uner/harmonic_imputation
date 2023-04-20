d9edc9f81da3b61fa4a434f30b23ec26fee4bc74
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2023-04-20 20:39:50 -0300

    Modified Error Computation Function

--- compute_errors.m
+++ compute_errors.m
@@ -1,28 +1,72 @@
-function [Tmse,mse,Tmae,mae,Trmse,rmse] = compute_errors(t,x,st,L)
-    K = length(L);
-
-    mse = zeros(1,K);
-    mae = zeros(1,K);
-    rmse = zeros(1,K);
-    int = [];
-    for k=1:K
-        Lk = L(k);
-        stk = st(k);
-        edk = stk + Lk - 1;
-        intk = stk:edk;
-        mse(k) = norm(t(intk)-x(intk))^2/Lk;
-        mae(k) = norm(t(intk)-x(intk),1)/Lk;
-        rmse(k) = sqrt(mse(k));
-        int = [int intk];
-
-
+function [Err,Err_i] = compute_errors(t,x,st,L,metric)
+if nargin<5
+    metric = 'mae';
+else
+    if ~iscell(metric)
+        metric = {metric};
     end
-    Tmse = norm(t(int)-x(int))^2/sum(L);
-    Tmae = norm(t(int)-x(int),1)/sum(L);
-    Trmse = sqrt(Tmse);
-
-
-
-
 end
+t = t(:);
+x = x(:);
 
+K = length(L);
+M = size(metric,2);
+N = length(t);
+
+Err_i = zeros(K,size(metric,1));
+Err = zeros(1,size(metric,1));
+for m=1:M
+    metr = metric{m};
+
+    switch metr
+        case 'mae'
+            mae = zeros(1,K);
+            for k=1:K
+                Lk = L(k);
+                stk = st(k);
+                edk = stk + Lk - 1;
+                intk = stk:edk;
+                mae(k) = norm(t(intk)-x(intk),1)/Lk;
+            end
+            Err_i(:,m) = mae;
+            Err(m) = sum(abs(t-x))/sum(L);
+        case 'mse'
+            mse = zeros(1,K);
+            for k=1:K
+                Lk = L(k);
+                stk = st(k);
+                edk = stk + Lk - 1;
+                intk = stk:edk;
+                mse(k) = norm(t(intk)-x(intk))^2/Lk;
+            end
+            Err_i(:,m) = mse;
+            Err(m) = sum((t-x).^2)/sum(L);
+        case 'rmse'
+            rmse = zeros(1,K);
+            for k=1:K
+                Lk = L(k);
+                stk = st(k);
+                edk = stk + Lk - 1;
+                intk = stk:edk;
+                rmse(k) = norm(t(intk)-x(intk))/Lk;
+            end
+            Err_i(:,m) = rmse;
+            Err(m) = sqrt(sum((t-x).^2)/sum(L));
+        case 'sim'
+            sim = zeros(1,K);
+            for k=1:K
+                Lk = L(k);
+                stk = st(k);
+                edk = stk + Lk - 1;
+                intk = stk:edk;
+                rk = max(t)-min(t);
+                sim(k) = sum(rk./(rk+abs(x(intk)-t(intk))))/Lk;
+
+            end
+            Err_i(:,m) = sim;
+            Err(m) = sum(rk./(rk+abs(x-t)))/sum(L);
+
+        otherwise
+            error(['Unknown Performance Metric "' metr '"'])
+    end
+end
