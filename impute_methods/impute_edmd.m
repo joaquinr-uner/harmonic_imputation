bfa127e02e60235522377cf0eda6ec618e66de35
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2023-06-26 10:50:18 -0300

    Added via Github Desktop

---
+++ impute_methods/impute_edmd.m
@@ -0,0 +1,96 @@
+function [s_imp] = impute_edmd(s,st,L,params)
+%%
+% Missing data imputation by forecasting using dynamical mode 
+% decomposition.
+% Inputs: 
+%         s: signal with missing data.
+%         st: initial indexes of missing data intervals.
+%         L: lengths of missing data intervals.         
+%         params: optional parameters struct. Includes:
+%                  K: size of the dataset for forecasting
+%                  M: lengths of the segments used for forecasting
+%                  sigma: kernel parameter
+% Outputs:
+%         s_imp: signal with imputed values.
+if nargin<4
+    K = 0;
+    M = 0;
+    %M = round(1*L);
+    %K = round(1.5*M);
+else
+    if isfield(params,'M')
+        M = params.M;
+        if length(M)==1
+            M = M*ones(1,length(L));
+        end
+    else
+        M = zeros(1,length(L));
+    end
+    if isfield(params,'K')
+        K = params.K;
+        if length(K)==1
+            K = K*ones(1,length(L));
+        end
+    else
+        K = round(2.5*M);
+    end
+    if isfield(params,'sigma')
+        sigma = params.sigma;
+    else
+        sigma = 100;
+    end
+end
+
+s = s(:);
+
+N = length(s);
+Ni = length(st);
+
+s_imp = s;
+
+intp = 0;
+for qi=1:Ni
+    inti = st(qi):st(qi)+L(qi)-1;
+    sp = s_imp(intp(end)+1:inti(1)-1);
+    Np = length(sp);
+    Ki = K(qi);
+    Mi = M(qi);
+    if Ki==0
+        [~,fh] = compute_sigma(sp,1);
+        Th = length(sp)/fh;
+        %Mi = floor(0.9*Th);
+        %Ki = ceil(3*Th);
+        Mi = ceil(3*Th);
+        Ki = floor(2.5*Mi);
+
+    end
+
+    if Ki+Mi>Np
+        Ki = floor(0.7*Np);
+        Mi = floor(0.3*Np)-1;
+    end
+
+    %sp = s_imp(1:inti(1)-1);
+
+    X = zeros(Mi,L(qi)) ;
+    for k = 1: Ki
+        X(:,k) = sp(end-Ki-Mi+k:end-Ki+k-1);
+    end
+    Y = [X(:,2:end) sp(end-Mi+1:end)] ;
+
+    [Xi,mu,phix] = approxKoopman(X,Y,sigma) ;
+
+    tmp = phix.';
+    
+    Z = zeros(Mi,L(qi));
+    for kk = 1:L(qi) % Eq. (4)
+        tmp = mu.*tmp;
+        Z(:,kk) =  tmp ;
+    end
+    Z = real(Xi.' * Z);
+    xext = Z(end,:)' ;
+
+
+    s_imp(inti) = xext;
+    intp = inti;
+end
---
+++ impute_methods/impute_lsw.m
@@ -0,0 +1,85 @@
+function [s_imp] = impute_lsw(s,st,L,params)
+%%
+% Missing data imputation by ARIMA model forward forecasting.
+% Inputs: 
+%         s: signal with missing data.
+%         st: initial indexes of missing data intervals.
+%         L: lengths of missing data intervals.
+%         params: optional parameters struct. Includes:
+%                 cycl: number of cycles used to fit ARIMA model
+%                 fmax: maximum STFT frequency, as proportion of sampling
+%                 frequency.
+%                 sigma: window parameter for STFT
+% Outputs:
+%         s_imp: signal with imputed values.
+if nargin<4
+    pn = cd;
+else
+    if isfield(params,'pn')
+        pn = params.pn;
+    else
+        pn = mfilename('fullpath');
+        pn = fullfile(pn(1:end-11), 'aux-functs');
+    end
+    if isfield(params,'T')
+        T = params.T;
+    else
+        T = 0;
+    end
+end
+s = s(:);
+
+Ni = length(st);
+
+s_imp = s;
+%if ~isfolder([pn '/temp'])
+%    mkdir([pn '/temp'])
+%end
+pn_temp = [pn '/temp/'];
+intp = 0;
+
+for qi=1:Ni
+
+    inti = st(qi):st(qi)+L(qi)-1;
+
+    sp = s_imp(1:inti(1)-1);
+
+    Left = max([1,st(qi)-3*L(qi)]);
+    si = s(Left:st(qi));
+    %si = sp;
+    Ni = length(si);
+    Li = L(qi);
+
+    
+    %saveR('temp/MissingSamples.R','si','T','sti','Li','Ni')
+    name = tempname(pn_temp);
+    saveR([name '.R'],'si','Li','Ni')
+    %CurrentDirectory=strrep(pwd,'\','/');
+
+   evalc(['!/usr/bin/Rscript ' pn '/RunLSW.R ' name '.R']);
+   %evalc(['!"C:\Program Files\R\R-3.6.3\bin\Rscript.exe" ' pn '/RunLSW.R ' name '.R']);
+
+    load([name '.R.mat'],'imp')
+        
+%    p = prctile(imp,95);
+% 
+%     for j=1:Li
+%         if imp(j)>p
+%             if j<Li 
+%                 if j>1
+%                     imp(j) = (imp(j-1)+imp(j+1))/2;
+%                 else
+%                     imp(j) = imp(j+1);
+%                 end
+%             else
+%                 imp(j) = imp(j-1);
+%             end
+%         end
+%     end
+    s_imp(inti) = imp - mean(imp);
+    intp = inti;
+end
+
+%warning('off')
+%rmdir(pn_temp,'s')
+%warning('on')
