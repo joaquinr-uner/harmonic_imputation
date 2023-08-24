9acc8f7b320aea8e4a74ed6d42397173e13a9097
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2023-08-24 13:01:38 -0300

    Misc. Corrections

--- impute_methods/impute_tbats.m
+++ impute_methods/impute_tbats.m
@@ -14,7 +14,7 @@ function [s_imp] = impute_tbats(s,st,L,params)
 %         s_imp: signal with imputed values.
 if nargin<4
     pn = cd;
-    T = 0;
+    Ti = 0;
     j = 0;
 else
     if isfield(params,'pn')
@@ -39,7 +39,7 @@ if ~isfolder([pn '/temp'])
 end
 pn_temp = [pn '/temp/'];
 intp = 0;
-  
+
 for qi=1:Ni
 
     inti = st(qi):st(qi)+L(qi)-1;
@@ -48,35 +48,39 @@ for qi=1:Ni
     sp = s(intp(end)+1:inti(1)-1);
 
     Np = length(sp);
-    [sigma,~,b] = compute_sigma(sp);
-
-    ff = 0:1/length(sp):0.5-1/length(sp);
-    [Ff,sFf] = STFT_Gauss(sp,Np,sigma,0.5);
-    Uf = istct_fast(Ff,ff,0.3,0.03);
-    Wf = Ff.*Uf;
-
-    cf = ridge_ext(Wf,0.1,0.1,10,10);
-    cf = ridge_correct(cf,Ff,b,1);
-
-    [~,phif] = extract_harmonics(Ff,sFf,cf,b,b,1);
-
-    wrapphi = wrapTo2Pi(2*pi*phif);dwphi = diff(wrapphi);
-    plocs = find(dwphi<-5);
-    pw = diff(plocs);
     if T == 0
+        [sigma,~,b] = compute_sigma(sp);
+
+        ff = 0:1/length(sp):0.5-1/length(sp);
+        [Ff,sFf] = STFT_Gauss(sp,Np,sigma,0.5);
+        Uf = istct_fast(Ff,ff,0.3,0.03);
+        Wf = Ff.*Uf;
+
+        cf = ridge_ext(Wf,0.1,0.1,10,10);
+        cf = ridge_correct(cf,Ff,b,1);
+
+        [~,phif] = extract_harmonics(Ff,sFf,cf,b,b,1);
+
+        wrapphi = wrapTo2Pi(2*pi*phif);dwphi = diff(wrapphi);
+        plocs = find(dwphi<-5);
+        pw = diff(plocs);
         if length(pw)<3
-            T = floor(median(pw));
+            Ti = floor(median(pw));
             ind = sum(pw);
         else
-            T = floor(median(pw(end-2:end)));
+            Ti = floor(median(pw(end-2:end)));
             ind = Np-sum(pw(end-2:end)):Np;
         end
+    else
+        ind = Np-3*T:Np;
     end
+    ind(ind<1) = [];
+
     si = sp(ind);
     Ni = length(si);
     sti = st(qi);
     Li = L(qi);
-    
+
     %saveR('temp/MissingSamples.R','si','T','sti','Li','Ni')
     name = tempname(pn_temp);
     saveR([name '.R'],'si','T','sti','Li','Ni','j')
@@ -85,11 +89,10 @@ for qi=1:Ni
     evalc(['!/usr/bin/Rscript ' pn '/RunTBATS.R ' name '.R']);
 
     load([name '.R.mat'],'imp')
-        
+
 
     s_imp(inti) = imp - mean(imp);
     intp = inti;
-    T = 0;
 end
 
 %warning('off')
